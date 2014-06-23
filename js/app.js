dc2586acb6957776dfe08801b3b68309708c375e
weilunallen <weilunallen@gmail.com>
2014-06-23 13:25:16 -0400

    initial

--- js/app.js
+++ js/app.js
@@ -1,3 +1,132 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+function expend(x) {
+	console.log(x);
+	$('.popDown').hide();
+	$(x).toggle();
+}
+function largeScreen(h,w) {
+	$('#main').height(
+		w*(9/16)
+	);	
+}
+function page(h,w) {
+	var page = $(".page");
+	var fullWidth = $(".fullWidth");
+	page.height(h);
+	page.width(w);
+	fullWidth.width(w);
+}
+function slideNav(h,w) {
+
+	var controller = $(".sandwich");
+	var subject = $(".view");
+	var distance = w*.8;
+	var speed = 500;
+	var sandwich = $("#mobileControl");
+	
+	window.slideStatus = "0"; // close
+	
+	
+	controller.click(
+		function() {
+		if(window.slideStatus === "0") {
+				subject.animate(
+					{
+						"left" : distance + "px"
+					}, speed, function() {
+						window.slideStatus = "1"
+					}
+				);
+				sandwich.animate(
+					{
+						"left" : distance + "px"
+					}, {
+						queue: false,
+						duration: speed, 
+						
+					}, function() {
+						window.slideStatus = "1"
+					}
+				);
+			} else if (window.slideStatus === "1") {
+				subject.animate(
+					{
+						"left" : "0px"
+					}, speed, function() {
+						window.slideStatus = "0"
+					}
+				);
+				sandwich.animate(
+					{
+						"left" : "0px"
+					}, {
+						queue: false,
+						duration: speed, 
+						
+					}, function() {
+						window.slideStatus = "0"
+					}
+				);
+			}
+		}
+	); // end click 
+	
+	$(".content").click(
+			function() {
+				if (window.slideStatus === "1") {
+				console.log("number");
+				controller.click();
+				
+				}
+			}
+		);
+	}
+
+function width80(w) {
+	var width80 = $(".width80");
+	var objWidth = width80.css("width");
+	var objWidth = objWidth.replace("px","");
+	var distanceToCenter = (w-objWidth*.8)/2;	
+	
+	console.log(w);
+	console.log(objWidth);
+	
+	
+	/*
+width80.css({
+		"width":w*.8+"px",
+		"marginLeft":distanceToCenter
+	});
+*/
+}
+function introAnimation() {
+	$(".keyFrame").each(
+		function(index) {
+			$(this).delay(350*index).animate(
+				{
+					opacity : 1
+				}, 800, function() {
+					
+				}
+			);
+		}
+	);
+}
+
+
+
+$(document).ready(
+	function() {
+		winHight = $(window).innerHeight();
+		winWidth = $(window).innerWidth();
+		if(winWidth > 64*16) {
+			largeScreen(winHight, winWidth);
+		} else {
+			page(winHight, winWidth); 		
+			introAnimation();	
+			slideNav(winHight, winWidth);
+		}
+	}
+);
---
+++ js/ng.js
@@ -0,0 +1,114 @@
+angular
+	.module("onePage", ["ngCookies","ngRoute"])
+	.controller("langCtrl", function langCtrl($scope, $cookies) 
+		{		
+			$scope.englishLibrary = function() {
+			// intro 
+			$scope.welcome = "WELCOME"
+			$scope.lang = "En";
+			$scope.dimsum = "DIM SUM";
+			$scope.seafood = "SEAFOOD";
+			$scope.poultry = "POULTRY";
+			$scope.banquet = "BANQUET";
+			$scope.map = "DIRECTION";
+			$scope.tel = "TELEPHONE";
+			// menu
+			$scope.menu = "MENU"
+			$scope.home = "HOME";
+			$scope.rsvp = "RSVP";
+			$scope.takeout = "TAKEOUT";
+			$scope.takeoutSmall = "ORDER ONLINE";
+			$scope.dinner = "DINNER";
+			$scope.contact = "CONTACT US";
+			$scope.rsvpSmall = "FOR 10+ GUESTS ONLY";
+			// 
+		}
+			$scope.chineseLibrary = function() {
+			$scope.welcome = "歡迎光臨"
+			$scope.lang = "中";
+			$scope.dimsum = "港式點心";
+			$scope.seafood = "生猛海味";
+			$scope.poultry = "山珍野味";
+			$scope.banquet = "豪華宴席";
+			$scope.map = "豪庭地圖";
+			$scope.tel = "豪庭電話";
+			// menu
+			$scope.home = "一覽";
+			$scope.home = "主頁";
+			$scope.rsvp = "定席";
+			$scope.takeout = "外賣";
+			$scope.takeoutSmall = "網上訂餐";
+			$scope.dinner = "晚餐";
+			$scope.contact = "聯絡我們";
+			$scope.rsvpSmall = "限十人以上";
+		}
+			
+			if(!$cookies.langCookies) {
+					$cookies.langCookies = "english"
+					$scope.englishLibrary();
+			} else {
+				if($cookies.langCookies === "english") {
+					$scope.englishLibrary();
+				} else if($cookies.langCookies === "chinese") {
+					$scope.chineseLibrary();
+				}
+			}
+			$scope.changeLang = function() {
+				if($cookies.langCookies === "english"
+				) {
+					$cookies.langCookies = "chinese";
+					$scope.chineseLibrary();
+				} else if($cookies.langCookies === "chinese"
+				) {
+					$cookies.langCookies = "english";
+					$scope.englishLibrary();
+				}
+			}	
+	})
+	 .controller('routeCtrl', function($scope, $route, $routeParams, $location,$cookies) {
+     	$scope.$route = $route;
+	 	$scope.$location = $location;
+	 	$scope.$routeParams = $routeParams;
+	 	$scope.params = $routeParams;
+	 	$scope.$on('$viewContentLoaded', function()
+	 	{
+        	page(winHight, winWidth); 		
+			introAnimation();	
+			slideNav(winHight, winWidth);		
+			
+			if($scope.params.setLang==="en") {
+			} 
+				
+		});
+	 })
+	 .config(function($routeProvider, $locationProvider) {
+		 $routeProvider
+		 	.when("/go",
+		 		{
+		 			templateUrl:"restOfstuff.html",
+		 			controller:"routeCtrl"
+		 		}
+		 	)
+		 	.when("/",
+		 		{
+		 			templateUrl:"html/intro.html",
+		 			controller:"routeCtrl"
+		 		}
+		 	)
+		 	.when("/:setLang/dimsum/",
+		 		{
+		 			templateUrl:"html/dimsum.html",
+		 			controller:"routeCtrl"
+		 		}
+		 	)
+	})
+	.controller('pageCtrl', function($scope, $cookies) {
+		$scope.next = function(lang, destination) {
+			
+		}
+	})
+	
+
+ 
+	
+ 
